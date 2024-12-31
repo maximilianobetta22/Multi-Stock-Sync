6e141e779979fe03b91a4486d7d56ad445b15727
astronautmarkus <astronautmarkus@gmail.com>
2024-12-31 10:27:17 -0300

    feat: implement Despacho view with content and footer actions

---
+++ multi-stock-sync/src/views/Despacho/Content/DespachoContent.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DespachoContent = () => {
+    return (
+        <div>
+            <h1>Despacho Content</h1>
+        </div>
+    );
+};
+
+export default DespachoContent;
--- multi-stock-sync/src/views/Despacho/Despacho.tsx
+++ multi-stock-sync/src/views/Despacho/Despacho.tsx
@@ -1,18 +1,57 @@
-import React from 'react';
 import PuntoVentaNavbar from '../../components/PuntoVentaNavbar/PuntoVentaNavbar';
+import DespachoContent from './Content/DespachoContent';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import './Despacho.css';
 
-const Despacho: React.FC = () => {
+const Despacho = () => {
     return (
         <>
-        <PuntoVentaNavbar />
-        <div>
-            <h1>Despacho</h1>
-            <p>Welcome to the Despacho page!</p>
-        </div>
-
+            <PuntoVentaNavbar />
+            <div className="despacho-container">
+                <div className="main-section">
+                    
+                </div>
+                <div className="sidebar">
+                    <DespachoContent />
+                </div>
+            </div>
+            <FooterActionsDespacho/>
         </>
     );
 };
+
+const FooterActionsDespacho = () => (
+    <div className="footer-actions-despacho">
+        <div className="footer-left-despacho">
+            <div className="footer-bottom">
+                <button className="footer-gray-button">
+                Cancelar <FontAwesomeIcon icon={faXmark} />
+                </button>
+                <button className="despachar-button">
+                    Despachar <FontAwesomeIcon icon={faCheck} />
+                </button>
+            </div>
+            <div className="totales">
+                <div className="totales-item">
+                    <span>Neto:</span>
+                    <span>$0</span>
+                </div>
+                <div className="totales-item">
+                    <span>Impuesto:</span>
+                    <span>$0</span>
+                </div>
+                <div className="totales-item total-grande">
+                    <span>Total:</span>
+                    <span>$0</span>
+                </div>
+            </div>
+        </div>
+        <div className="footer-right-despacho">
+
+        </div>
+    </div>
+);
 
 export default Despacho;
