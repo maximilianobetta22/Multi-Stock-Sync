13bc05cb7f358b4603c2365595a0fd3d19f7e2f4
astronautmarkus <astronautmarkus@gmail.com>
2025-01-02 18:49:16 -0300

    feat: replace Navbar with AdminNavbar and update layout in Documentos component

--- multi-stock-sync/src/views/Admin/Documentos/Documentos.tsx
+++ multi-stock-sync/src/views/Admin/Documentos/Documentos.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import Navbar from '../../../components/Navbar/Navbar';
+import AdminNavbar from '../../../components/AdminNavbar/AdminNavbar';
 import './Documentos.css';
 
 const Documentos: React.FC = () => {
     return (
         <>
-            <Navbar />
-            <div className="documentos-dashboard">
-                <h1>Documentos</h1>
+            <AdminNavbar />
+            <div className="d-flex flex-grow-1 main-container">
+                <div className="w-50 bg-light p-3 d-flex align-items-center justify-content-center">
+                    <div>
+                        <h1>Contenido Izquierdo</h1>
+                        <p>Aquí va el contenido principal del lado izquierdo.</p>
+                    </div>
+                </div>
+                <div className="w-50 custom-gray p-3 d-flex align-items-center justify-content-center">
+                    <div>
+                        <h1>Contenido Derecho</h1>
+                        <p>Aquí va el contenido principal del lado derecho.</p>
+                    </div>
+                </div>
             </div>
         </>
     );
