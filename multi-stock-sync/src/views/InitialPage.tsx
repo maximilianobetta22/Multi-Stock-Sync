5979037c1644b45ed3a3c256cf0427f4c99e0382
astronautmarkus <astronautmarkus@gmail.com>
2025-01-02 09:12:58 -0300

    feat: update InitialPage layout and styles for improved navigation and aesthetics

--- multi-stock-sync/src/views/InitialPage.tsx
+++ multi-stock-sync/src/views/InitialPage.tsx
@@ -11,26 +11,35 @@ function InitialPage() {
       <div className="custom-background d-flex justify-content-center align-items-center">
         <div className="container text-center">
           <div className="row align-items-center">
+
+            {/* Arrow */}
+            <div className="col-md-2 text-muted">
+              <FontAwesomeIcon icon={faArrowRight} className="fa-2x" />
+              <p>Atiende al publico</p>
+            </div>
+
             {/* Punto de Venta */}
-            <div className="col-md-5">
+            <div className="col-md-4">
               <Link to="/punto-venta">
                 <FontAwesomeIcon icon={faDesktop} className="fa-4x text-primary" />
-                <h3 className="mt-3">PUNTO DE VENTA</h3>
-                <p className="mt-3 text-muted">Atiende al público</p>
+                <h3 className="mt-3 text-muted">PUNTO DE VENTA</h3>
+              </Link>
+            </div>
+
+            {/* Documentos */}
+            <div className="col-md-4">
+              <Link to="/documentos">
+                <FontAwesomeIcon icon={faFileInvoiceDollar} className="fa-4x text-warning" />
+                <h3 className="mt-3 text-muted">DOCUMENTOS</h3>
               </Link>
             </div>
 
             {/* Arrow */}
             <div className="col-md-2 text-muted">
-              <FontAwesomeIcon icon={faArrowRight} className="fa-2x" />
+              <FontAwesomeIcon icon={faArrowRight} className="fa-2x rotate-180" />
+              <p>Genera facturas, notas de venta, etc.</p>
             </div>
 
-            {/* Documentos */}
-            <div className="col-md-5">
-              <FontAwesomeIcon icon={faFileInvoiceDollar} className="fa-4x text-warning" />
-              <h3 className="mt-3">DOCUMENTOS</h3>
-              <p className="mt-3 text-muted">Genera facturas, notas de venta, etc.</p>
-            </div>
           </div>
         </div>
       </div>
