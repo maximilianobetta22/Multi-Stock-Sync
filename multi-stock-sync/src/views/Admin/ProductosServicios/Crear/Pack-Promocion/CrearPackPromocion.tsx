97ab022efb002f0274f30a482278661ec8948b43
astronautmarkus <astronautmarkus@gmail.com>
2025-01-06 11:00:55 -0300

    feat: enhance CrearPackPromocion layout with vertical split and updated content

--- multi-stock-sync/src/views/Admin/ProductosServicios/Crear/Pack-Promocion/CrearPackPromocion.tsx
+++ multi-stock-sync/src/views/Admin/ProductosServicios/Crear/Pack-Promocion/CrearPackPromocion.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import AdminNavbar from '../../../../../components/AdminNavbar/AdminNavbar';
 import './CrearPackPromocion.css';
 
 const CrearPackPromocion: React.FC = () => {
     return (
         <>
-            <AdminNavbar />
             <div className="d-flex flex-grow-1 main-container">
-                <div className="w-100 bg-light p-3 d-flex align-items-center justify-content-center">
+                <div className="w-50 bg-light p-3 d-flex align-items-center justify-content-center">
                     <div>
-                        <h1>Crear pack / promocion</h1>
+                        <h1>Contenido Izquierdo</h1>
+                        <p>Aquí va el contenido principal del lado izquierdo.</p>
+                        <p>Componente: Pack promocion</p>
+                    </div>
+                </div>
+                <div className="w-50 custom-gray p-3 d-flex flex-column">
+                    <div className="v-25 p-3 d-flex align-items-center justify-content-center">
+                        <div>
+                            <h1>Contenido Superior Derecho</h1>
+                            <p>Aquí va el contenido superior del lado derecho.</p>
+                        </div>
+                    </div>
+                    <div className="v-25 p-3 d-flex align-items-center justify-content-center">
+                        <div>
+                            <h1>Contenido Inferior Derecho</h1>
+                            <p>Aquí va el contenido inferior del lado derecho.</p>
+                        </div>
                     </div>
                 </div>
             </div>
