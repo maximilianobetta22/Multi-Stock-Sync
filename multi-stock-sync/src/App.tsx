e80f006f24bc84306c64b397f99386f86a16a7cb
Nicolás Hernández <nico.hernandezekko@gmail.com>
2025-01-07 09:16:06 -0300

    feature(Regsiter.tsx): Se agregaron nuevos estilos

--- multi-stock-sync/src/App.tsx
+++ multi-stock-sync/src/App.tsx
@@ -20,37 +20,35 @@ import CrearPackPromocion from './views/Admin/ProductosServicios/Crear/Pack-Prom
 import AdminReportes from './views/Admin/Reportes/Reportes';
 import Stock from './views/Admin/Stock/Stock';
 
-
 import Login from './views/Auth/Login/Login';
 import Register from './views/Auth/Register/Register';
 
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/" element={<InitialPage />} />
-      <Route path="/punto-venta" element={<PuntoVentaDashboard />} />
-      <Route path="/punto-venta/despacho" element={<Despacho />} />
-      <Route path="/punto-venta/reimprimir" element={<Reimprimir />} />
-      <Route path="/punto-venta/abono-cliente" element={<AbonoCliente />} />
-      <Route path="/punto-venta/devolucion" element={<Devolucion />} />
-      <Route path="/punto-venta/movimientos-efectivo" element={<MovimientosEfectivo />} />
-      <Route path="/punto-venta/cierre-caja" element={<CierreCaja />} />
-      <Route path="/punto-venta/reportes" element={<Reportes />} />
-      <Route path="/punto-venta/maestros" element={<Maestros />} />
-      <Route path="/punto-venta/recepcion-stock" element={<RecepcionStock />} />
-      <Route path="/admin/documentos" element={<Documentos />} />
-      <Route path="/admin/productos-servicios" element={<ProductosServicios />} />
-      <Route path="/admin/productos-servicios/crear/producto" element={<CrearProducto />} />
-      <Route path="/admin/productos-servicios/crear/servicio" element={<CrearServicio />} />
-      <Route path="/admin/productos-servicios/crear/pack-promocion" element={<CrearPackPromocion />} />
-      <Route path="/admin/reportes" element={<AdminReportes />} />
-      <Route path="/admin/stock" element={<Stock />} />
-    </Routes>
-  </Router>
-
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<InitialPage />} />
+        <Route path="/punto-venta" element={<PuntoVentaDashboard />} />
+        <Route path="/punto-venta/despacho" element={<Despacho />} />
+        <Route path="/punto-venta/reimprimir" element={<Reimprimir />} />
+        <Route path="/punto-venta/abono-cliente" element={<AbonoCliente />} />
+        <Route path="/punto-venta/devolucion" element={<Devolucion />} />
+        <Route path="/punto-venta/movimientos-efectivo" element={<MovimientosEfectivo />} />
+        <Route path="/punto-venta/cierre-caja" element={<CierreCaja />} />
+        <Route path="/punto-venta/reportes" element={<Reportes />} />
+        <Route path="/punto-venta/maestros" element={<Maestros />} />
+        <Route path="/punto-venta/recepcion-stock" element={<RecepcionStock />} />
+        <Route path="/admin/documentos" element={<Documentos />} />
+        <Route path="/admin/productos-servicios" element={<ProductosServicios />} />
+        <Route path="/admin/productos-servicios/crear/producto" element={<CrearProducto />} />
+        <Route path="/admin/productos-servicios/crear/servicio" element={<CrearServicio />} />
+        <Route path="/admin/productos-servicios/crear/pack-promocion" element={<CrearPackPromocion />} />
+        <Route path="/admin/reportes" element={<AdminReportes />} />
+        <Route path="/admin/stock" element={<Stock />} />
+      </Routes>
+    </Router>
   );
 }
 
--- multi-stock-sync/src/views/PuntoVenta/RecepcionStock/RecepcionStock.tsx
+++ multi-stock-sync/src/views/PuntoVenta/RecepcionStock/RecepcionStock.tsx
@@ -3,18 +3,18 @@ import './RecepcionStock.css';
 import PuntoVentaNavbar from '../../../components/PuntoVentaNavbar/PuntoVentaNavbar';
 
 const RecepcionStock: React.FC = () => {
-    return (
-        <>
-            <PuntoVentaNavbar />
-            <div className="d-flex flex-grow-1 main-container">
-                <div className="w-100 bg-light p-3 d-flex align-items-center justify-content-center">
-                    <div>
-                        <h1>Recepcion stock</h1>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <PuntoVentaNavbar />
+      <div className="d-flex flex-grow-1 main-container">
+        <div className="w-100 bg-light p-3 d-flex align-items-center justify-content-center">
+          <div>
+            <h1>Recepcion stock</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default RecepcionStock;
