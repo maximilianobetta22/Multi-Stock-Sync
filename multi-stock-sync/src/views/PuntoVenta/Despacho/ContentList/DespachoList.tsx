cb196d7fa561a87fdfd1a11c65b42718f0259a67
garvi <gary.villegasb@gmail.com>
2025-01-08 13:34:04 -0300

    Cambiar Css a Module, se trata de hacer funcional tabla en despacho

---
+++ multi-stock-sync/src/views/PuntoVenta/Despacho/ContentList/DespachoList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DespachoList: React.FC = () => {
+  return (
+    <div>
+      <h1>¡Bienvenido a DespachoList!</h1>
+      <p>Esta es tu nueva vista en React.</p>
+    </div>
+  );
+};
+
+export default DespachoList;
--- multi-stock-sync/src/views/PuntoVenta/Despacho/Despacho.tsx
+++ multi-stock-sync/src/views/PuntoVenta/Despacho/Despacho.tsx
@@ -1,24 +1,23 @@
-import PuntoVentaNavbar from '../../../components/PuntoVentaNavbar/PuntoVentaNavbar';
-import DespachoContent from './Content/DespachoContent';
-import './Despacho.css';
+import PuntoVentaNavbar from "../../../components/PuntoVentaNavbar/PuntoVentaNavbar";
+import DespachoContent from "./Content/DespachoContent";
+import DespachoList from "./ContentList/DespachoList";
 
 const Despacho: React.FC = () => {
-    return (
-        <>
-            <PuntoVentaNavbar />
-            <div className="d-flex flex-grow-1 main-container">
-                <div className="w-70 bg-light p-3 d-flex align-items-center justify-content-center">
-                    <div>
-                        <h1>Contenido Izquierdo</h1>
-                        <p>Aquí va el contenido principal del lado izquierdo.</p>
-                    </div>
-                </div>
-                <div className="w-30 custom-gray p-3 d-flex align-items-center justify-content-center">
-                    <DespachoContent />
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <PuntoVentaNavbar />
+      <div className="d-flex flex-grow-1 main-container">
+        <div className="w-70 bg-light p-3 d-flex align-items-center justify-content-center">
+          <div>
+            <DespachoList />
+          </div>
+        </div>
+        <div className="w-30 custom-gray p-3 d-flex align-items-center justify-content-center">
+          <DespachoContent />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Despacho;
