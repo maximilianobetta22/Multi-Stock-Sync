524b008ec4b120f2019ae23f1daf91b18eace8e4
astronautmarkus <astronautmarkus@gmail.com>
2025-01-10 12:53:52 -0300

    Add CrearProducto component and update routing

--- multi-stock-sync/src/views/Sync/views/Productos/Router/RouterProducto.tsx
+++ multi-stock-sync/src/views/Sync/views/Productos/Router/RouterProducto.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
+import CrearProducto from "../Views/Crear/CrearProducto";
+
 function RouterProductos() {
 
   return (
     <Routes>
       <Route path="/home" element={<div>Landing Page</div>} />
-      <Route path="/crear" element={<div>Crear</div>} />
+      <Route path="/crear" element={<CrearProducto />} />
 
       <Route path="/*" element={<Navigate to="/sync/productos/home"/>}/>
     </Routes>
