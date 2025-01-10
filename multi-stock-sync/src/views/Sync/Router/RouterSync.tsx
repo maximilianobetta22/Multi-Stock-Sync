6f50a2446fcf9f85cf02d0ce54453ff43b47e58f
Bryan747474 <adrimatu77@gmail.com>
2025-01-10 12:26:03 -0300

    creacion index

--- multi-stock-sync/src/views/Sync/Router/RouterSync.tsx
+++ multi-stock-sync/src/views/Sync/Router/RouterSync.tsx
@@ -1,17 +1,21 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import LayoutSync from "../Layout/LayoutSync";
+import Index from "../Views/Index";
 
 function RouterSync() {
   
   return (
     <LayoutSync>
       <Routes>
+        
+        <Route path="/menu" element={<Index/>} />
         <Route path="/perfil" element={<div>Perfil</div>} />
         <Route path="/productos" element={<div>Productos</div>} />
         <Route path="/conexiones" element={<div>Conexiones</div>} />
         <Route path="/woocommerce" element={<div>WooCommerce</div>} />
 
-        <Route path="/*" element={<Navigate to="/sync/perfil"/>}/>
+
+        <Route path="/*" element={<Navigate to="/sync/menu"/>}/>
       </Routes>
     </LayoutSync>
   );
---
+++ multi-stock-sync/src/views/Sync/Views/Index/Index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './Index.module.css';
+
+const Index: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Sync Page</h1>
+            <p>This is a basic React page calling Index.module.css</p>
+        </div>
+    );
+};
+
+export default Index;
