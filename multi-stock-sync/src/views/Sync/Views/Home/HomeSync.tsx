406a5b86ec2ddabece62a623e4a9f4e3907a0e3a
astronautmarkus <astronautmarkus@gmail.com>
2025-01-10 13:29:30 -0300

    Add HomeSync component and update RouterSync for new home route

---
+++ multi-stock-sync/src/views/Sync/Views/Home/HomeSync.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeSync: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to HomeSync</h1>
+            <p>This is the home page for the Multi-Stock Sync application.</p>
+        </div>
+    );
+};
+
+export default HomeSync;
