3dabcd44123a90e727bcb4d1973c7af52bd6a81c
Gary Villegas <gary.villegasb@gmail.com>
2025-01-14 13:07:55 -0300

    Build: Se borra Conexiones

--- multi-stock-sync/src/views/Sync/Router/RouterSync.tsx
+++ multi-stock-sync/src/views/Sync/Router/RouterSync.tsx
@@ -15,7 +15,6 @@ function RouterSync() {
         <Route path="/perfil" element={<div>Perfil</div>} />
         <Route path="/productos/*" element={<RouterProductos />} />
         <Route path="/bodegas/*" element={<RouterBodegas />} />
-        <Route path="/conexiones" element={<div>Conexiones</div>} />
         <Route path="/woocommerce" element={<div>WooCommerce</div>} />
         <Route path="/loginMercadoLibre" element={<LoginMercado />} />
 
