7c4b03a14ffa70ae3cb75c6aa8ef59921a6768e3
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-17 10:12:39 -0300

    feat: enhance ToastComponent with z-index for improved visibility

--- multi-stock-sync/src/views/Sync/Components/ToastComponent/ToastComponent.tsx
+++ multi-stock-sync/src/views/Sync/Components/ToastComponent/ToastComponent.tsx
@@ -8,13 +8,13 @@ interface ToastComponentProps {
 
 const ToastComponent: React.FC<ToastComponentProps> = ({ message, type, onClose }) => {
     return (
-        <div className={`toast show position-fixed bottom-0 end-0 m-3 bg-${type}`} role="alert" aria-live="assertive" aria-atomic="true">
+        <div className={`toast show position-fixed bottom-0 end-0 m-3 bg-${type}`} role="alert" aria-live="assertive" aria-atomic="true" style={{ zIndex: 1050 }}>
             <div className={`toast-header bg-${type}`}>
-                <strong className="me-auto" style={{ color: 'white' }}>MultiStock-Sync</strong>
-                <button type="button" className="btn-close" onClick={onClose}></button>
+            <strong className="me-auto" style={{ color: 'white' }}>MultiStock-Sync</strong>
+            <button type="button" className="btn-close" onClick={onClose}></button>
             </div>
             <div className="toast-body" style={{ backgroundColor: 'white', color: 'black' }}>
-                {message}
+            {message}
             </div>
         </div>
     );
