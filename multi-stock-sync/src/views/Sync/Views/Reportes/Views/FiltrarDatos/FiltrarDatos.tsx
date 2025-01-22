525733c32a1d1b37bf3bacfbc6b70f3373c092f9
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-22 11:19:36 -0300

    feat: add RouterReportes with routes for FiltrarDatos and ExportarDatos

---
+++ multi-stock-sync/src/views/Sync/Views/Reportes/Views/FiltrarDatos/FiltrarDatos.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './FiltrarDatos.module.css';
+
+const FiltrarDatos: React.FC = () => {
+    return (
+        <div>
+            <h1>FiltrarDatos Datos</h1>
+            <p>Esta es la página de filtrar de datos.</p>
+        </div>
+    );
+};
+
+export default FiltrarDatos;
