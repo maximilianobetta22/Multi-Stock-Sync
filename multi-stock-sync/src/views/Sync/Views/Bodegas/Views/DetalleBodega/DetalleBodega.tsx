8def9f70398ce314690ad6b92c9856f7cf11a443
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-22 17:43:34 -0300

    Refactor: Update DetalleBodega styles and improve product table layout

--- multi-stock-sync/src/views/Sync/Views/Bodegas/Views/DetalleBodega/DetalleBodega.tsx
+++ multi-stock-sync/src/views/Sync/Views/Bodegas/Views/DetalleBodega/DetalleBodega.tsx
@@ -1,23 +1,8 @@
 import styles from './DetalleBodega.module.css';
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-//import { Link } from 'react-router-dom';
 import { LoadingDinamico } from "../../../../../../components/LoadingDinamico/LoadingDinamico";
-//Esto es el loading dinamico
-//import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
-//import {  faArrowDown } from '@fortawesome/free-solid-svg-icons';
-/*
-interface Product {
-  id: number;
-  title: string;
-  category_code: string;
-  price_clp: number;
-  stock_mercado_libre: number;
-  warehouse_stock: number;
-  bodega_asignada:string;
-  image_url: string;
-  mercadolibre_url: string;
-}*/
+
 interface Product {
   id: number;
   thumbnail: string;
@@ -29,7 +14,6 @@ interface Product {
   created_at: string;
   updated_at: string;
 }
-//POR AHORA DE COMPANIA SOLO SACAMOS EL NAME
 
 interface Company {
   id: number;
@@ -47,11 +31,7 @@ interface Warehouse {
   updated_at: string;
   company: Company;
 }
-/*
-interface ApiResponse {
-  message: string;
-  data: Warehouse;
-}*/
+
 interface ApiResponseWarehouse {
   message: string;
   data: Warehouse;
@@ -60,12 +40,15 @@ interface ApiResponseProducts {
   message: string;
   data: Product[];
 }
+
 const DetalleBodega = () => {
   const { id } = useParams<{ id: string }>();
   const [warehouse, setWarehouse] = useState<Warehouse | null>(null);
   const [products, setProducts] = useState<Product[]>([]);
   const [error, setError] = useState<string | null>(null);
   const [searchTerm, setSearchTerm] = useState("");
+
+  const placeholderImage = "/assets/img/icons/image_notfound.svg";
 
   useEffect(() => {
     const fetchWarehouse = async () => {
@@ -107,14 +90,15 @@ const DetalleBodega = () => {
     fetchWarehouse();
     fetchProducts();
   }, [id]);
-  /*FILTRO EXPERIMENTAL DE PRODUCTOS */
+
   const filteredProducts = products.filter((product) => {
     return (
-      product.title.toLowerCase().includes(searchTerm.toLowerCase()) || // Filtra por título
-      product.id.toString().includes(searchTerm) || // Filtra por ID
-      product.id_mlc.toLowerCase().includes(searchTerm.toLowerCase()) // Filtra por ID MLC
+      product.title.toLowerCase().includes(searchTerm.toLowerCase()) || 
+      product.id.toString().includes(searchTerm) || 
+      product.id_mlc.toLowerCase().includes(searchTerm.toLowerCase())
     );
   });
+
   if (error) {
     return <p>Error: {error}</p>;
   }
@@ -124,78 +108,49 @@ const DetalleBodega = () => {
   }
 
   return (
-    <div className={styles.tableContainer}>
-      <div className={styles.header}>
-        <h1 className="mt-2 mb-2">Atributos de Bodega </h1>
-      </div>
-
-      {/* Condición para mostrar los detalles */}
-      
-        <table className={styles.table}>
-          <thead>
+    <div>
+      <div className="container-fluid">
+        <h1 className="mt-2 mb-2">Atributos de Bodega</h1>
+        <div className={styles.tableContainer}>
+          <table className={styles.table}>
+            <thead>
               <tr>
                 <th className="table_header">ID</th>
-                <th className="table_header">Nomrbe</th>
-                <th className="table_header">Ubicacion</th>
-                <th className="table_header">Nombre Compañia</th>
-                <th className="table_header">Fecha de creacion</th>
-                <th className="table_header">Fecha de modificacion</th>
-                
+                <th className="table_header">Nombre</th>
+                <th className="table_header">Ubicación</th>
+                <th className="table_header">Nombre Compañía</th>
+                <th className="table_header">Fecha de creación</th>
+                <th className="table_header">Fecha de modificación</th>
               </tr>
-            </thead>        
-          <tbody>
-          <tr key={warehouse.id}>
-                
+            </thead>
+            <tbody>
+              <tr key={warehouse.id}>
                 <td>{warehouse.id}</td>
                 <td>{warehouse.name}</td>
                 <td>{warehouse.location}</td>
                 <td>{warehouse.company.name}</td>
                 <td>{new Date(warehouse.created_at).toLocaleDateString("es-CL")}</td>
                 <td>{new Date(warehouse.updated_at).toLocaleDateString("es-CL")}</td>
-                
               </tr>
+            </tbody>
+          </table>
+        </div>
 
-          </tbody>
-        </table>
-      
-      <div className={`${styles.header} ${styles.tableContainer2}`}>
-        {/*Tabla "inspirada" por la tabla en HomeProducto.tsx */}
         <h1 className="mt-2 mb-2">Productos en la Bodega</h1>
-        
-        {products.length === 0 ? (
-          <p></p>
-        ) : (
-        
         <input
-                  type="text"
-                  placeholder="Buscar por ID, Título o Código de Categoría"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  style={{
-                    marginBottom: "10px",
-                    padding: "5px",
-                    width: "100%",
-                    maxWidth: "500px",
-                  }}/>
-                )}
-        {products.length === 0 ? (
-          <p>No hay productos registrados en esta bodega.</p>
-        ) : (
-          
-          <table className={styles.table} >
-          {/* 
-          <input
-                  type="text"
-                  placeholder="Buscar por ID, Título o Código de Categoría"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  style={{
-                    marginBottom: "10px",
-                    padding: "5px",
-                    width: "100%",
-                    maxWidth: "500px",
-                  }}/>  */}
-            
+          type="text"
+          placeholder="Buscar por ID, Título o Código de Categoría"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          style={{
+            marginBottom: "10px",
+            padding: "5px",
+            width: "100%",
+            maxWidth: "500px",
+          }}
+        />
+        <div className={styles.tableContainer}>
+          <table className={styles.table}>
             <thead>
               <tr>
                 <th className="table_header">Imágen</th>
@@ -210,35 +165,39 @@ const DetalleBodega = () => {
                 <th className="table_header">Fecha Actualización</th>
               </tr>
             </thead>
-            <tbody className="tbody2">
-              {filteredProducts.map((product) => (
-                <tr key={product.id}>
-                  <td>
-                    <img
-                      src={product.thumbnail}
-                      alt={product.title}
-                      style={{ width: "50px", height: "50px" }}
-                    />
-                  </td>
-                  <td>{product.id}</td>
-                  <td>{product.title}</td>
-                  <td>{product.id_mlc}</td>
-                  <td>{product.price_clp}</td>
-                  <td>{product.warehouse_stock}</td>
-                  <td>{product.warehouse_id}</td>
-                  <td>{product.warehouse_stock}</td>
-                  <td>{new Date(product.created_at).toLocaleDateString("es-CL")}</td>
-                  <td>{new Date(product.updated_at).toLocaleDateString("es-CL")}</td>
-                  
+            <tbody>
+              {filteredProducts.length > 0 ? (
+                filteredProducts.map((product) => (
+                  <tr key={product.id}>
+                    <td>
+                      <img
+                        src={product.thumbnail}
+                        alt={product.title}
+                        style={{ width: "50px", height: "50px" }}
+                        onError={(e) => (e.currentTarget.src = placeholderImage)}
+                      />
+                    </td>
+                    <td>{product.id}</td>
+                    <td>{product.title}</td>
+                    <td>{product.id_mlc}</td>
+                    <td>{new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(Number(product.price_clp))}</td>
+                    <td>{product.warehouse_stock}</td>
+                    <td>{product.warehouse_id}</td>
+                    <td>{product.warehouse_stock}</td>
+                    <td>{new Date(product.created_at).toLocaleDateString("es-CL")}</td>
+                    <td>{new Date(product.updated_at).toLocaleDateString("es-CL")}</td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan={10} className="text-muted">No hay productos registrados en esta bodega.</td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
-        )}
+        </div>
       </div>
-      
     </div>
-     
   );
 };
 
