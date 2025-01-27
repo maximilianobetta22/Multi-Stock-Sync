a91675570c1aef54da0384c2a2c6fdb0ddfffcb0
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-27 15:38:30 -0300

    feat: add PDF export functionality and user data fetching in IngresosSemana component

--- multi-stock-sync/src/views/Sync/Views/Reportes/Views/IngresosSemana/IngresosSemana.tsx
+++ multi-stock-sync/src/views/Sync/Views/Reportes/Views/IngresosSemana/IngresosSemana.tsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
+
 import { Bar } from "react-chartjs-2";
 import { ChartOptions } from "chart.js";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { LoadingDinamico } from "../../../../../../components/LoadingDinamico/LoadingDinamico";
+
+import { Modal } from "react-bootstrap";
+
+import { jsPDF } from "jspdf";
+import autoTable from "jspdf-autotable";
 
 const IngresosSemana: React.FC = () => {
   const { client_id } = useParams<{ client_id: string }>();
+  const navigate = useNavigate();
 
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
+
+  const [userData, setUserData] = useState<{ nickname: string; profile_image: string } | null>(null);
+
   const [year, setYear] = useState<string>('');
   const [month, setMonth] = useState<string>('');
   const [weeks, setWeeks] = useState<{ start_date: string; end_date: string }[]>([]);
@@ -33,6 +43,9 @@ const IngresosSemana: React.FC = () => {
       },
     ],
   });
+
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [pdfDataUrl, setPdfDataUrl] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchWeeks = async () => {
@@ -187,6 +200,147 @@ const IngresosSemana: React.FC = () => {
       },
     },
   };
+  /* urls ventas por mes  */
+  const handleNavigate = () => {
+    navigate(`/sync/reportes/ventas-mes/${client_id}`);
+  };
+
+  /* fin de urls ventas por mes  */
+
+  /* llamar al api */
+  const fetchUserData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/mercadolibre/credentials/${client_id}`);
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`Error del servidor: ${errorText}`);
+      }
+      const result = await response.json();
+      console.log("Resultado:", result);
+      setUserData({
+
+        nickname: result.nickname,
+        profile_image: result.profile_image,
+      });
+    } catch (error: any) {
+      console.error("Error al obtener los datos del usuario:", error.message);
+      setError(error.message || "Hubo un problema al cargar los datos del usuario.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (client_id) {
+      fetchUserData();
+    }
+  }, [client_id]);
+
+  /* fin de llamar a la api */
+
+  /* pdf */
+  const generatePDF = (): void => {
+    const doc = new jsPDF();
+
+    if (userData) {
+      doc.setFont("helvetica", "normal");
+      doc.setFontSize(14);
+      doc.setTextColor(0, 0, 0); // Negro
+      doc.text(`Usuario: ${userData.nickname}`, 20, 55);
+    }
+
+    if (userData && userData.profile_image) {
+      const imageUrl = userData.profile_image;
+      const imgWidth = 30; // Ancho de la imagen
+      const imgHeight = 30; // Altura de la imagen
+      const xPosition = 160; // Posición X
+      const yPosition = 40; // Posición Y
+
+      const image = new Image();
+      image.src = imageUrl;
+      image.onload = () => {
+        doc.addImage(image, "JPEG", xPosition, yPosition, imgWidth, imgHeight);
+        doc.save("reporte_semanal.pdf"); // Guarda el PDF después de agregar la imagen
+      };
+    }
+
+
+    // Función para obtener el nombre del mes en español
+    const getMonthName = (monthNumber: number): string => {
+      const months: string[] = [
+        "enero", "febrero", "marzo", "abril", "mayo", "junio",
+        "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+      ];
+      return months[monthNumber - 1]; // Restar 1 porque los meses son 1-indexados en este caso
+    };
+
+    doc.setFont("helvetica", "bold");
+    doc.setFontSize(20);
+    doc.text("Reporte Semanal de Ingresos", 105, 20, { align: "center" });
+    doc.setDrawColor(0, 0, 0);
+    doc.line(20, 25, 190, 25);
+    doc.setFontSize(12);
+
+    // Datos del reporte: Año, Mes (nombre) y Semana
+    if (selectedWeek && month && year) {
+      const monthNumber = parseInt(month);
+      const monthName: string = getMonthName(monthNumber);
+      doc.text(`Año: ${year}`, 20, 35);
+      doc.text(`Mes: ${monthName}`, 20, 42);
+      doc.text(`Semana: ${selectedWeek}`, 20, 49);
+    }
+
+    if (totalSales !== null) {
+      doc.setFont("helvetica", "bold");
+      doc.setFontSize(16);
+      doc.setTextColor(34, 139, 34); // Verde
+      doc.text(`Total de Ingresos: $${totalSales.toLocaleString()}`, 20, 60);
+    }
+
+    autoTable(doc, {
+      startY: 70,
+      headStyles: {
+        fillColor: [41, 128, 185],
+        textColor: [255, 255, 255],
+        fontSize: 12,
+        halign: "center",
+      },
+      bodyStyles: {
+        fontSize: 10,
+        textColor: [0, 0, 0],
+      },
+      alternateRowStyles: {
+        fillColor: [240, 240, 240],
+      },
+      head: [["Producto", "Ingresos Totales", "Cantidad Vendida"]],
+      body: chartData.labels.map((label: string, index: number) => [
+        label,
+        `$${chartData.datasets[0].data[index]}`,
+        chartData.datasets[1].data[index],
+      ]),
+    });
+
+    const pageHeight = doc.internal.pageSize.height;
+    doc.setFontSize(10);
+    doc.setTextColor(150, 150, 150); // Gris claro
+    doc.text("----------Multi Stock Sync----------", 105, pageHeight - 10, {
+      align: "center",
+    });
+
+    const pdfData = doc.output("datauristring");
+    setPdfDataUrl(pdfData);
+    setShowModal(true);
+  };
+
+
+  /* fin del pdf */
+
+
+
+
+
+
 
   return (
     <>
@@ -262,6 +416,26 @@ const IngresosSemana: React.FC = () => {
                   <h2>Ingreso Semanal: ${totalSales.toLocaleString()}</h2>
                 </div>
               )}
+
+              <button
+                type="button"
+                className="btn btn-secondary"
+                onClick={handleNavigate}
+              >
+                Ir a Ventas por Mes
+              </button>
+              <br />
+
+              <button
+                type="button"
+                className="btn btn-success mt-3"
+                onClick={generatePDF}
+                disabled={chartData.labels.length === 0}
+              >
+                Exportar a PDF
+              </button>
+
+
             </div>
             <div className="col-md-8">
               <Bar data={chartData} options={chartOptions} />
@@ -269,6 +443,26 @@ const IngresosSemana: React.FC = () => {
           </div>
         </div>
       )}
+      <Modal show={showModal} onHide={() => setShowModal(false)} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Reporte Semanal de Ingresos</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {pdfDataUrl && (
+            <iframe
+              src={pdfDataUrl}
+              width="100%"
+              height="500px"
+              title="Reporte Semanal de Ingresos"
+            />
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <button className="btn btn-secondary" onClick={() => setShowModal(false)}>
+            Cerrar
+          </button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 };
