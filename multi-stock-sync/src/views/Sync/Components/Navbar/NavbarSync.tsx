84e71c049f54a41f39c006f22def8ea6fe53b8f4
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-28 17:34:15 -0300

    refactor: update Navbar styles and remove unused dropdown menu

--- multi-stock-sync/src/views/Sync/Components/Navbar/NavbarSync.tsx
+++ multi-stock-sync/src/views/Sync/Components/Navbar/NavbarSync.tsx
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import styles from "./NavbarSync.module.css";
 
 const Navbar = () => {
@@ -11,37 +9,6 @@ const Navbar = () => {
           <img src="/assets/img/logo/logo-blanco-text.svg" alt="Logo" className={styles.logoImage} />
         </Link>
         <div className="d-flex align-items-center ms-auto">
-
-          <div className="dropdown">
-            <button
-              className={`btn ${styles.iconButton}`}
-              id="dropdownMenuButton"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <FontAwesomeIcon icon={faEllipsisV} />
-            </button>
-            <ul
-              className={`dropdown-menu dropdown-menu-end ${styles.dropdownMenu}`}
-              aria-labelledby="dropdownMenuButton"
-            >
-              <li>
-                <Link to="/sync/productos" className="dropdown-item">
-                  Productos
-                </Link>
-              </li>
-              <li>
-                <Link to="/sync/bodegas" className="dropdown-item">
-                  Bodegas
-                </Link>
-              </li>
-              <li>
-                <Link to="/admin" className="dropdown-item text-danger">
-                  Salir a inicio
-                </Link>
-              </li>
-            </ul>
-          </div>
         </div>
       </div>
     </nav>
