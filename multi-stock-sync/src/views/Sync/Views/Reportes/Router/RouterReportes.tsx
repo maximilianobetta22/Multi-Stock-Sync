65bc497f7dcb92712fb1fc172c2cb3d76631ad1f
Marcos Reyes <astronautmarkus@gmail.com>
2025-01-31 16:23:26 -0300

    feat: remove ExportarDatos and FiltrarDatos components and their styles wip

--- multi-stock-sync/src/views/Sync/Views/Reportes/Router/RouterReportes.tsx
+++ multi-stock-sync/src/views/Sync/Views/Reportes/Router/RouterReportes.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import FiltrarDatos from "../Views/FiltrarDatos/FiltrarDatos";
-import ExportarDatos from "../Views/ExportarDatos/ExportarDatos";
+
 import HomeReportes from "../Views/Home/HomeReportes";
 import VentasPorDia from "../Views/VentasPorDia/VentasPorDia";
 import VentasPorMes from "../Views/VentasPorMes/VentasPorMes";
@@ -24,8 +23,7 @@ function RouterReportes() {
         <Routes>
             <Route path="/*" element={<Navigate to="/sync/reportes/home" />} />  
             <Route path="home" element={<HomeReportes/>} />
-            <Route path="filtrar-datos/:client_id" element={<FiltrarDatos />} />
-            <Route path="exportar-datos/:client_id" element={<ExportarDatos />} />
+
             <Route path="ingreso-semana/:client_id" element={< IngresosSemana/>} />
             <Route path="ventas-dia/:client_id" element={<VentasPorDia />} />
             <Route path="ventas-mes/:client_id" element={<VentasPorMes />} />
