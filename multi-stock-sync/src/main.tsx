e2bc23933e361c48d014b84e2f261f4a9553982b
Marcos Reyes <astronautmarkus@gmail.com>
2025-02-14 16:50:52 -0300

    fix: Switch from HashRouter to BrowserRouter in main.tsx

--- multi-stock-sync/src/main.tsx
+++ multi-stock-sync/src/main.tsx
@@ -8,7 +8,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter> {/* Changed HashRouter to BrowserRouter */}
       <App />
     </BrowserRouter>
   </StrictMode>,
