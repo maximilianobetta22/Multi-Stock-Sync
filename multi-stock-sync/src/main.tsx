b296f64b2daa93fa4462c3c251989f90578d9fee
franarredondom <fran.arredondom@gmail.com>
2025-04-21 18:09:22 -0400

    Actualización HomeProducto - HomeReportes

--- multi-stock-sync/src/main.tsx
+++ multi-stock-sync/src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from 'react-router-dom'; // Changed import
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import App from './App.tsx'
+import "antd/dist/reset.css"; // Importante: CSS base de Ant Design
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
