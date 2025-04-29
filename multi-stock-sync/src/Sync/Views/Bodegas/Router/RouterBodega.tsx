73784f03ee3cd4a27507f68874effbd802ae4511
franarredondom <fran.arredondom@gmail.com>
2025-04-29 11:04:13 -0400

    Update RouterBodega.tsx

--- multi-stock-sync/src/Sync/Views/Bodegas/Router/RouterBodega.tsx
+++ multi-stock-sync/src/Sync/Views/Bodegas/Router/RouterBodega.tsx
@@ -1,13 +1,12 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
 import HomeBodega from "../Views/Home/HomeBodega";
-import EditarBodega from "../Views/Editar/EditarBodega";
 
 function RouterBodegas() {
   return (
     <Routes>
       <Route path="/home" element={<HomeBodega />} />
-      <Route path="/editar/:id" element={<EditarBodega />} />
+     
       <Route path="/*" element={<Navigate to="/sync/bodegas/home" />} />
     </Routes>
   );
